34a24a64d600977ea088bc7717d380ac9b1d7ebc
Alex <lialin163@yandex.ru>
2022-11-12 11:43:55 +0300

    GetSizeSecondArray

--- itogovaya.cs
+++ itogovaya.cs
@@ -1,4 +1,5 @@
 ﻿int size = 6;
+int n = 3;
 string[] arrayOne = new string[size];
 Console.WriteLine($"Введите набор символов, означающих значение строкового элемента массива, {size} раз через enter, без пробелов");
 FillArray(arrayOne);
@@ -21,4 +22,16 @@ void PrintArray (string[] arr)
         Concole.Write (arr[i] + " ");
     }
     Concole.Write ("]");
+}
+
+void GetSizeSecondArray(string[] arr)
+{
+    int secondSize = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if(arr[i].Length <= n)
+        {
+            secondSize++;
+        }
+    }
 }
