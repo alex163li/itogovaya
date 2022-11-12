f6385c6e2c844a1cb9bc5bba5c4904406ad31afd
Alex <lialin163@yandex.ru>
2022-11-12 11:06:50 +0300

    data entry

--- itogovaya.cs
+++ itogovaya.cs
@@ -1,1 +1,24 @@
-﻿
+﻿int size = 6;
+string[] arrayOne = new string[size];
+Console.WriteLine($"Введите набор символов, означающих значение строкового элемента массива, {size} раз через enter, без пробелов");
+FillArray(arrayOne);
+Console.Clear();
+PrintArray(arrayOne);
+
+void FillArray (string[] arr)
+{
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = Console.ReadLine() ?? "";
+    }
+}
+
+void PrintArray (string[] arr)
+{
+    Concole.Write ("[ ");
+    for (int i = 0; i < size; i++)
+    {
+        Concole.Write (arr[i] + " ");
+    }
+    Concole.Write ("]");
+}
